745513bd3fa93d6ddf561209104d3545f79032a9
Enrique Alcantara <enriquealc27@gmail.com>
2023-05-27 09:30:18 -0400

    Fix tests

--- src/services/CreateInvoiceService.test.ts
+++ src/services/CreateInvoiceService.test.ts
@@ -1,6 +1,6 @@
 import { DB } from "sqlite";
 import { beforeEach, describe, it } from "testing/bdd.ts";
-import { assertEquals } from 'testing/asserts.ts';
+import { assertEquals } from "testing/asserts.ts";
 import InitInvoicerService from "./InitInvoicerService.ts";
 import CreateInvoiceService, { InvoiceData } from "./CreateInvoiceService.ts";
 import { createDB } from "../infra/index.ts";
@@ -15,7 +15,7 @@ describe("CreateInvoiceService", () => {
     type: "B02",
   };
   const invoiceData: InvoiceData = {
-    creationDate: '2022-01-01',
+    creationDate: "01-01-2022",
     paymentDeadline: 15,
     dopExchangeRate: 10,
     entries: [
@@ -59,33 +59,39 @@ describe("CreateInvoiceService", () => {
     assertEquals(createdInvoice.customer, customer);
   });
 
-  it('assigns default business to the invoice', () => {
+  it("assigns default business to the invoice", () => {
     assertEquals(createdInvoice.business, business);
   });
 
-  it('assigns default invoice author to the invoice', () => {
+  it("assigns default invoice author to the invoice", () => {
     assertEquals(createdInvoice.author, invoiceAuthor);
   });
 
-  it('assigns the first unused NCF to the invoice', () => {
+  it("assigns the first unused NCF to the invoice", () => {
     assertEquals(createdInvoice.ncf, { ...unusedNCF, used: true });
   });
 
-  it('assigns the creation date to the invoice', () => {
-    assertEquals(createdInvoice.creationDate, new Date(invoiceData.creationDate));
+  it("assigns the creation date to the invoice", () => {
+    assertEquals(
+      createdInvoice.creationDate,
+      new Date(invoiceData.creationDate)
+    );
   });
 
-  it('assigns the payment deadline to the invoice', () => {
+  it("assigns the payment deadline to the invoice", () => {
     assertEquals(createdInvoice.paymentDeadline, invoiceData.paymentDeadline);
   });
 
-  it('assigns the entries to the invoice', () => {
-    assertEquals(createdInvoice.entries, invoiceData.entries.map((entry) => ({
-      ...entry
-    })));
+  it("assigns the entries to the invoice", () => {
+    assertEquals(
+      createdInvoice.entries,
+      invoiceData.entries.map((entry) => ({
+        ...entry,
+      }))
+    );
   });
 
-  it('sets unused ncf as used', () => {
+  it("sets unused ncf as used", () => {
     assertEquals(sut.ncfStore.findById(unusedNCF.code)?.used, true);
   });
 });
